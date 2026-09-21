dd1c616d7e7682a2deb47bb103f967777bf73d98
Mikhail <mikhail.ivanyuta@gmail.com>
2026-09-21 22:07:58 +0300

    Revert "Temp disable buying tariff"
    This reverts commit 0c2f19544961d65403d44089a5e49c6fbb215737.

--- app/handlers/subscription/tariff_purchase.py
+++ app/handlers/subscription/tariff_purchase.py
@@ -847,9 +847,6 @@ async def select_tariff(
     state: FSMContext,
 ):
     """Обрабатывает выбор тарифа."""
-    await callback.answer('Будет доступно позже', show_alert=True)
-    return
-
     tariff_id = int(callback.data.split(":")[1])
     tariff = await get_tariff_by_id(db, tariff_id)
 
